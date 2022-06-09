f3258c86ff0eb1af2218e620c16495d1f9dc2617
lsz <lishengz.cool@hotmail.com>
2022-06-09 09:55:14 +0800

    add topcis

--- app/market_aggregate/sh/create_topics.sh
+++ app/market_aggregate/sh/create_topics.sh
@@ -1,22 +1,36 @@
 # !/bin/bash
 
-bin/kafka-topics.sh  --create --topic DEPTH.BTC_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic DEPTH.ETH_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic DEPTH.BTC_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic DEPTH.ETH_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic DEPTH.ETH_BTC._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic DEPTH.USDT_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic DEPTH.BTC_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic DEPTH.ETH_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic DEPTH.XRP_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic DEPTH.ETH_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic DEPTH.ETH_BTC._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic DEPTH.USDT_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
 
-bin/kafka-topics.sh  --create --topic TRADE.BTC_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic TRADE.ETH_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic TRADE.BTC_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic TRADE.ETH_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic TRADE.ETH_BTC._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic TRADE.USDT_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic TRADE.BTC_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic TRADE.ETH_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic TRADE.XRP_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic TRADE.ETH_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic TRADE.ETH_BTC._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic TRADE.USDT_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
 
-bin/kafka-topics.sh  --create --topic KLINE.BTC_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic KLINE.ETH_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic KLINE.BTC_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic KLINE.ETH_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic KLINE.ETH_BTC._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
-bin/kafka-topics.sh  --create --topic KLINE.USDT_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic KLINE.BTC_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic KLINE.ETH_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic KLINE.XRP_USDT._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic KLINE.ETH_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic KLINE.ETH_BTC._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+# bin/kafka-topics.sh  --create --topic KLINE.USDT_USD._bcts_ --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+
+bin/kafka-topics.sh  --create --topic DEPTH.BTC_USDT.FTX --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+bin/kafka-topics.sh  --create --topic DEPTH.ETH_USDT.FTX --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+bin/kafka-topics.sh  --create --topic DEPTH.XRP_USDT.FTX --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+
+
+bin/kafka-topics.sh  --create --topic TRADE.BTC_USDT.FTX --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+bin/kafka-topics.sh  --create --topic TRADE.ETH_USDT.FTX --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+bin/kafka-topics.sh  --create --topic TRADE.XRP_USDT.FTX --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+
+
+bin/kafka-topics.sh  --create --topic KLINE.BTC_USDT.FTX --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+bin/kafka-topics.sh  --create --topic KLINE.ETH_USDT.FTX --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
+bin/kafka-topics.sh  --create --topic KLINE.XRP_USDT.FTX --bootstrap-server 10.10.1.45:9092  --replication-factor 1 --partitions 1
