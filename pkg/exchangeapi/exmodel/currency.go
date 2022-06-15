fce183138fa47aef75f526a2e9639a0384799af9
hutu1st <546017527@qq.com>
2022-06-16 00:03:27 +0800

    opu update

--- pkg/exchangeapi/exmodel/currency.go
+++ pkg/exchangeapi/exmodel/currency.go
@@ -1,9 +1,11 @@
 package exmodel
 
+import "strings"
+
 type Currency string
 
 func NewCurrency(s string) Currency {
-	return Currency(s)
+	return Currency(strings.ToUpper(s))
 }
 
 func (o Currency) String() string {
