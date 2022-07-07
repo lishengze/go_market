99a22073b7e1ad7edf0da770b1deb9e5e6e47edf
lishengze <lishengz.cool@hotmail.com>
2022-07-07 16:09:09 +0800

    update sc

--- app/monitor/clean.sh
+++ app/monitor/clean.sh
@@ -2,11 +2,11 @@
 
 rm -fr log/*.log
 
-PID=`ps aux|grep front |grep -v grep | awk '{print $2}'`
+PID=`ps aux|grep monitor |grep -v grep | awk '{print $2}'`
 kill -9 $PID
 
-rm -fr front
+rm -fr monitor
 
 sleep 2s
 
-ps -aux|grep front
+ps -aux|grep monitor
