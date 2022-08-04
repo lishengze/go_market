b9b43339f2f40b09677a977c8bc02b0dd8f100a7
lishengze <lishengz.cool@hotmail.com>
2022-08-04 12:40:10 +0800

    update

--- common/datastruct/cache_kline.go
+++ common/datastruct/cache_kline.go
@@ -370,19 +370,22 @@ func (k *KlineCache) InitCacheKline(new_kline *Kline, resolution int) *Kline {
 	kline := NewKlineWithKline(new_kline)
 	kline.SetPerfectTime(int64(resolution))
 	kline.Resolution = resolution
-	kline.Volume = 0
 	kline.LastVolume = 0
+
+	pub_kline = NewKlineWithKline(kline)
+
+	if new_kline.IsHistory() {
+		if IsOldKlineEnd(new_kline, int64(resolution)) {
+			k.AddCompletedKline(kline, resolution)
+		}
+	} else {
+		kline.Volume = 0
+	}
 
 	k.SetCacheKline(kline, resolution)
 	k.SetLastKline(new_kline, resolution)
 
 	logx.Slowf("InitCache: %s", kline.FullString())
-
-	if new_kline.IsHistory() && IsOldKlineEnd(new_kline, int64(resolution)) {
-		k.AddCompletedKline(kline, resolution)
-	}
-
-	pub_kline = kline
 
 	return pub_kline
 }
